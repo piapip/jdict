e08726c499d8b3dc9e1baa2e780f628e9d51b67f
piapip <tho.ph153619@sis.hust.edu.vn>
2020-04-04 03:22:14 +0700

    Setting up workplace. Finish the edit button.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import FindButton from './component/FindButton'
+import ShowWord from './component/showWord/ShowWord'
+import AddButton from './component/AddButton'
+import EditButton from './component/EditButton'
+import ShowRandomButton from './component/ShowRandomButton'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    dictionary: [
+      {
+        id: 1,
+        kanji: '犬',
+        furigana: 'inu',
+        meaning: 'dog'
+      },
+      {
+        id: 2,
+        kanji: '猫',
+        furigana: 'neko',
+        meaning: 'cat'
+      },
+      {
+        id: 3,
+        kanji: '鳥',
+        furigana: 'tori',
+        meaning: 'bird'
+      },
+      {
+        id: 4,
+        kanji: '犬',
+        furigana: 'inu',
+        meaning: 'dog'
+      }
+    ],
+    idGenerator: 5,
+    showFurigana: true,
+    showMeaning: true,
+    displayWordID: 1
+  }
+
+  edit = (newKanji, newFurigana, newMeaning) => {
+    let dictionary = this.state.dictionary
+    let index = dictionary.findIndex((element) => {
+      return element.id = this.state.displayWordID
+    })
+    dictionary[index] = {
+      id: this.state.displayWordID,
+      kanji: newKanji,
+      furigana: newFurigana,
+      meaning: newMeaning
+    }
+    this.setState({ dictionary })
+  }
+
+  delete = () => {
+    let dictionary = this.state.dictionary
+    dictionary = dictionary.filter((element) => {
+      return element.id !== this.state.displayWordID
+    })
+    this.setState({ dictionary: dictionary, displayWordID: dictionary[0].id})
+  }
+
+  render() {
+    var displayWord = this.state.dictionary.find(((element) => {
+      return element.id === this.state.displayWordID
+    }))
+
+    return (
+      <div className="App">
+        <FindButton /> {/* Nguyen */}
+        <AddButton /> {/* Hung */}
+        <EditButton 
+          displayWord = {displayWord} 
+          editWord = {this.edit}
+          deleteWord = {this.delete}/> {/* Tho */}
+        <ShowWord 
+          displayWord = {displayWord}
+          showFurigana = {this.state.showFurigana}
+          showMeaning = {this.state.showMeaning}/> {/* Hand + Tu */}
+        <ShowRandomButton /> {/* Nguyen */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/ShowRandomButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowRandomButton extends Component {
+  render() {
+    return (
+      <div>
+            
+      </div>
+    );
+  }
+}
+
+export default ShowRandomButton;
